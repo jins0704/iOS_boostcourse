febd0f79174e011a1a395268278aeafcb7a223e2
hongjinseok <sldkdl111@ajou.ac.kr>
2021-01-05 23:30:49 +0900

    view SignUp

---
+++ 2.SignUp/2.SignUp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  2.SignUp
+//
+//  Created by 홍진석 on 2021/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
