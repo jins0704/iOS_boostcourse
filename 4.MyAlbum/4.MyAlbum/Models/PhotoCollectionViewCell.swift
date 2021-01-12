7511cba5cb56139b66756f57326eb5f56dcc7ad0
hongjinseok <sldkdl111@ajou.ac.kr>
2021-01-12 12:58:32 +0900

    try2 4.MyAlbum

--- 4.MyAlbum/4.MyAlbum/Models/PhotoCollectionViewCell.swift
+++ 4.MyAlbum/4.MyAlbum/Models/PhotoCollectionViewCell.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
-   
+
     @IBOutlet weak var imageView: UIImageView!
+    
     override func awakeFromNib() {
            super.awakeFromNib()
            // Initialization code
--- 4.MyAlbum/4.MyAlbum/ViewController.swift
+++ 4.MyAlbum/4.MyAlbum/ViewController.swift
@@ -11,85 +11,119 @@ import Photos
 
 class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate{
     
-    @IBOutlet weak var photocollection: UICollectionView!
-    var allPhotos : [PHFetchResult<PHAsset>] = [] // 앨범 정보
-    var imageManager = PHCachingImageManager() // 앨범에서 받아오기 위한 객체
-    var fetchOptions: PHFetchOptions {
-        let fetchOptions = PHFetchOptions()
-       fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-       return fetchOptions
-    }//옵션
-    var photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
-    var flowLayout : UICollectionViewFlowLayout!
+    @IBOutlet weak var collectionview: UICollectionView!
+    
+    //앨범 정보
+    var fetchResult =
+        [PHFetchResult<PHAsset>]()
+    let imageManager = PHCachingImageManager()
+    var cameraRoll : PHFetchResult<PHAssetCollection>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
+        collectionview.dataSource = self
+        collectionview.delegate = self
+        
         settingFlowlayout()
-        check()
-        
-        photocollection.dataSource = self
-        photocollection.delegate = self
-        
+        checkPermission()
+
+        self.collectionview.reloadData()
     }
     
-    func check(){
-        switch self.photoAuthorizationStatus{
+    //permission 확인
+    func checkPermission(){
+        
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+        switch photoAuthorizationStatus{
         case .authorized :
             print("authorized")
             self.requestCollection()
+            self.collectionview.reloadData()
         case .denied :
             print("denied")
-        case .notDetermined :
-            print("notDetermined")
+        case .notDetermined:
+            print("notdetermined.")
+            PHPhotoLibrary.requestAuthorization() {
+            (status) in
+            switch status {
+            case .authorized:
+                print("User permiited")
+                self.requestCollection()
+                
+                //비동기적으로 실행
+                OperationQueue.main.addOperation {
+                    self.collectionview.reloadData()
+                }
+            case .denied:
+                print("User denied")
+                break
+            default:
+                break
+            }
+        }
         case .restricted:
             print("restricted")
+            
         default : break
         }
     }
     
+    //layout 설정
     func settingFlowlayout(){
-        self.flowLayout = UICollectionViewFlowLayout()
+        let flowLayout = UICollectionViewFlowLayout()
         flowLayout.sectionInset = UIEdgeInsets.zero
         flowLayout.minimumLineSpacing = 10
         flowLayout.minimumInteritemSpacing = 10
         
         let half : CGFloat = UIScreen.main.bounds.width / 2.0
-        flowLayout.estimatedItemSize = CGSize(width: half-30, height: 90)
-        self.photocollection.collectionViewLayout = flowLayout
+        flowLayout.itemSize = CGSize(width: half+20, height: 50)
+        
+        self.collectionview.collectionViewLayout = flowLayout
     }
     
     func requestCollection(){
-        let cameraRoll = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
         
-        addAlbums(collection: cameraRoll)
+        //앨범 목록
+        let Album = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: nil)
+        print(Album.count)
+       
+        let AlbumList = Album.objects(at: IndexSet(0..<Album.count))
         
-        OperationQueue.main.addOperation {
-                self.photocollection.reloadData()
+        //정렬 - 최신순
+        let fetchOption = PHFetchOptions()
+        fetchOption.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        //목록 다 가져와서 fetchResult에 저장
+        for i in 0..<Album.count{
+            fetchResult.append(PHAsset.fetchAssets(in: AlbumList[i], options: fetchOption))
         }
-        
-        fetchOptions.sortDescriptors = [NSSortDescriptor(key : "creationDate", ascending: false)]
+        print(fetchResult.count)
     }
     
-    private func addAlbums(collection : PHFetchResult<PHAssetCollection>){
-        for i in 0 ..< collection.count {
-            let collection = collection.object(at: i)
-            self.allPhotos.append(PHAsset.fetchAssets(in: collection, options: fetchOptions))
-        }
-    }
+    //CollectionView
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return allPhotos.count
+        //return self.fetchResult?.count ?? 0
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = photocollection.dequeueReusableCell(withReuseIdentifier: "cell" , for: indexPath) as! PhotoCollectionViewCell
-        guard let asset = allPhotos[indexPath.row].firstObject else{
+        //cell
+        guard let cell : PhotoCollectionViewCell = collectionview.dequeueReusableCell(withReuseIdentifier: "cell" , for: indexPath) as? PhotoCollectionViewCell else{
+            print("cell error")
             return UICollectionViewCell()
         }
-       
-        imageManager.requestImage(for: asset, targetSize: CGSize(width: 50, height: 50), contentMode: .aspectFill, options: nil, resultHandler: {image, _ in cell.imageView.image = image})
+        
+        let asset: PHAsset = fetchResult[indexPath.item].object(at: 0)
+        //사진 요청
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: 30, height: 30), contentMode: .aspectFill, options: nil, resultHandler: {asset, _ in
+            //cell.imageView?.image = asset
+            
+        })
+        
+        //이미지 잘 가져왔는지 확인차
         cell.backgroundColor = UIColor.black
         
         return cell
